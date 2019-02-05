29f5e800d3442a8a9c61b2d07aecde21df658089
Thomas Legrand <legrand.thomas5@hotmail.fr>
2019-02-05 20:32:05 +0100

    WIP

---
+++ Extractor.ts
@@ -0,0 +1,11 @@
+import { isProbablyReaderable } from "./Readability-readerable";
+import * as Readability from "./Readability";
+import {JSDOM} from "jsdom";
+
+function main(url: string) {
+    var doc = new JSDOM("<body>Here's a bunch of text</body>", {
+        url: "https://www.example.com/the-page-i-got-the-source-from",
+    });
+    let reader = new Readability(doc.window.document, null);
+    let article = reader.parse();
+}
